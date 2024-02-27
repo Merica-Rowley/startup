f8ed33a5f8300330112d9025e01b6fed31ed3cf6
Merica-Rowley <mericarowley@gmail.com>
2024-02-27 15:05:46 -0700

    implemented daily leaderboard reset functionality

--- scores.js
+++ scores.js
@@ -60,9 +60,28 @@ function loadAllTimeScoreboard() {
     }
 }
 
+function resetDailyBoard() {
+    localStorage.removeItem("dailyScores");
+    localStorage.setItem("dailyScores", "");
+}
+
+// Display of user welcome (exists across all pages)
 const playerWelcomeElement = document.querySelector("#user-welcome");
 playerName = localStorage.getItem("username") ?? "";
 playerWelcomeElement.textContent = "Welcome " + playerName + "!";
 
+// Display leaderboard values from local storage
 loadDailyScoreboard();
 loadAllTimeScoreboard();
+
+// Reset the daily leaderboard at 23:59, only the hours and minutes on this Date object matter, since they are what will be compared
+let resetLimitTime = new Date(2024, 1, 1, 23, 58, 59, 999);
+
+// Checks every one second to see if the current time is past 23:58; if so, daily leaderboard is reset
+setInterval(function () {
+    let now = new Date();
+
+    if ((now.getHours() > resetTime.getHours()) && (now.getMinutes() > resetTime.getMinutes())) {
+        resetDailyBoard();
+    }
+}, 1000)
