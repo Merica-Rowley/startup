6724a04be0644ee96ecc656eb4b0726fdd45b426
Merica Rowley <mericarowley@gmail.com>
2024-03-21 11:11:08 -0600

    added controls for play/logout to home page

--- public/home.js
+++ public/home.js
@@ -1,13 +1,27 @@
+(async () => {
+    const playerWelcomeElement = document.querySelector("#user-welcome");
+    playerName = localStorage.getItem("username") ?? "";
+    if (playerName) {
+        playerWelcomeElement.textContent = "Welcome " + playerName + "!";
+    } else {
+        playerWelcomeElement.textContent = "Welcome!";
+    }
+})();
+
 function login() {
     const nameElement = document.querySelector("#usernameInput");
     localStorage.setItem("username", nameElement.value);
-    window.location.href("quiz.html");
+    window.location.href = 'quiz.html'; // TODO: fix this; doesn't go to quiz.html, not sure why
 }
 
-const playerWelcomeElement = document.querySelector("#user-welcome");
-playerName = localStorage.getItem("username") ?? "";
-if (playerName) {
-    playerWelcomeElement.textContent = "Welcome " + playerName + "!";
-} else {
-    playerWelcomeElement.textContent = "Welcome!";
+function create() {
+
+}
+
+function logout() {
+
+}
+
+function quiz() {
+    window.location.href = 'quiz.html';
 }
