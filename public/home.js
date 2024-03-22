059bcc0d73ca4084185dac8dc55035ce1bbf1ef8
Merica Rowley <mericarowley@gmail.com>
2024-03-21 22:18:10 -0600

    implemented user creation in index.js; auth token is stored in cookies when user is created

--- public/home.js
+++ public/home.js
@@ -1,3 +1,4 @@
+// Set "Welcome user!" element in top right
 (async () => {
     const playerWelcomeElement = document.querySelector("#user-welcome");
     playerName = localStorage.getItem("username") ?? "";
@@ -8,43 +9,68 @@
     }
 })();
 
+// Show or hide login/create or play/logout buttons
 (async () => {
     const username = localStorage.getItem('username');
     if (username) {
-        const loginFormElement = document.querySelector("#login-form");
+        const loginFormElement = document.querySelector('#login-form');
         if (loginFormElement) {
             loginFormElement.style.display = 'none';
         }
 
-        const playButtonsElement = document.querySelector("#play-buttons");
+        const playButtonsElement = document.querySelector('#play-buttons');
         if (playButtonsElement) {
             playButtonsElement.style.display = 'block';
         }
     } else {
-        const loginFormElement = document.querySelector("#login-form");
+        const loginFormElement = document.querySelector('#login-form');
         if (loginFormElement) {
             loginFormElement.style.display = 'block';
         }
 
-        const playButtonsElement = document.querySelector("#play-buttons");
+        const playButtonsElement = document.querySelector('#play-buttons');
         if (playButtonsElement) {
             playButtonsElement.style.display = 'none';
         }
     }
 })();
 
-function login() {
-    const nameElement = document.querySelector("#usernameInput");
-    localStorage.setItem("username", nameElement.value);
-    window.location.href = 'quiz.html'; // TODO: fix this; doesn't go to quiz.html, not sure why
+async function login() {
+    loginOrCreate('/api/login');
 }
 
-function create() {
+async function create() {
+    loginOrCreate('/api/create');
+}
 
+async function loginOrCreate(endpoint) {
+    const username = document.querySelector('#usernameInput')?.value;
+    const password = document.querySelector('#passwordInput')?.value;
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    });
+
+    if (response.ok) {
+        localStorage.setItem('username', username);
+        window.location.href = 'quiz.html';
+    } else {
+        const body = await response.json();
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
+    }
 }
 
 function logout() {
     localStorage.removeItem('username');
+    fetch('/api/logout', {
+        method: 'delete'
+    }).then(() => (window.href.location = 'index.html'));
 }
 
 function quiz() {
