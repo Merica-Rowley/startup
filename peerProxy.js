215739f58363d4ff3eae40cd5334f3e7a762714d
Merica Rowley <mericarowley@gmail.com>
2024-03-26 18:03:50 -0600

    installed WebSocket with npm install

--- peerProxy.js
+++ peerProxy.js
@@ -1,4 +1,6 @@
 const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
 
-function peerProxy(httpServer) { }
+function peerProxy(httpServer) {
+    console.log("yep, I'm connected");
+}
