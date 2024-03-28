4542a4178de9fca0a521575c949abe1fc0b8cfed
Merica Rowley <mericarowley@gmail.com>
2024-03-27 20:40:28 -0600

    updated name display functions in info.js and scores.js; fixed issue with WebSocket message display in quiz.html

--- public/info.js
+++ public/info.js
@@ -1,7 +1,18 @@
-const playerWelcomeElement = document.querySelector("#user-welcome");
-playerName = localStorage.getItem("username") ?? "";
-if (playerName) {
-    playerWelcomeElement.textContent = "Welcome " + playerName + "!";
-} else {
-    playerWelcomeElement.textContent = "Welcome!";
-}
+// const playerWelcomeElement = document.querySelector("#user-welcome");
+// playerName = localStorage.getItem("username") ?? "";
+// if (playerName) {
+//     playerWelcomeElement.textContent = "Welcome " + playerName + "!";
+// } else {
+//     playerWelcomeElement.textContent = "Welcome!";
+// }
+
+// Set "Welcome user!" element in top right
+(async () => {
+    const playerWelcomeElement = document.querySelector("#user-welcome");
+    playerName = localStorage.getItem("username") ?? "";
+    if (playerName) {
+        playerWelcomeElement.textContent = "Welcome " + playerName + "!";
+    } else {
+        playerWelcomeElement.textContent = "Welcome!";
+    }
+})();
--- public/scores.js
+++ public/scores.js
@@ -86,15 +86,16 @@ function displayImage() {
         });
 }
 
-
-// Display of user welcome (exists across all pages)
-const playerWelcomeElement = document.querySelector("#user-welcome");
-playerName = localStorage.getItem("username") ?? "";
-if (playerName) {
-    playerWelcomeElement.textContent = "Welcome " + playerName + "!";
-} else {
-    playerWelcomeElement.textContent = "Welcome!";
-}
+// Set "Welcome user!" element in top right
+(async () => {
+    const playerWelcomeElement = document.querySelector("#user-welcome");
+    playerName = localStorage.getItem("username") ?? "";
+    if (playerName) {
+        playerWelcomeElement.textContent = "Welcome " + playerName + "!";
+    } else {
+        playerWelcomeElement.textContent = "Welcome!";
+    }
+})();
 
 // Load and display leaderboard values
 loadDailyScoreboard();
